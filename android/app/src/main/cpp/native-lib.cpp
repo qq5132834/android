6bb4891fd182b9cd9ff35c8eaf622b6953559551
qq5132834 <513283439@qq.com>
2024-03-31 18:24:32 +0800

    测试 ffmpeg 的依赖是否成功，测试ffmpeg的版本号。

--- android/app/src/main/cpp/native-lib.cpp
+++ android/app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,20 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring
 
-JNICALL
-Java_com_example_livestream_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+extern "C" {
+
+    #include "libavutil/avutil.h"
+    JNIEXPORT jstring
+
+
+    JNICALL
+    Java_com_example_livestream_MainActivity_stringFromJNI(
+            JNIEnv *env,
+            jobject /* this */) {
+        std::string hello = "Hello from C++, ffmpeg version: ";
+        hello += av_version_info();
+        return env->NewStringUTF(hello.c_str());
+    }
 }
+
