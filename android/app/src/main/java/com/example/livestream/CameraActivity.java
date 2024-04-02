d1dbb21b3634db176d5452e74a98784d9caf7f62
qq5132834 <513283439@qq.com>
2024-04-02 23:57:09 +0800

    打开摄像头

--- android/app/src/main/java/com/example/livestream/CameraActivity.java
+++ android/app/src/main/java/com/example/livestream/CameraActivity.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.livestream.databinding.ActivityCameraBinding;
+
 public class CameraActivity extends AppCompatActivity {
+
+    private ActivityCameraBinding activityCameraBinding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        this.activityCameraBinding = ActivityCameraBinding.inflate(getLayoutInflater());
+
         System.out.println(this.getClass().getName());
     }
 }
